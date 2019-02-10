461ad4d941564e30d846159d208c55a98007fa59
Jaime Estéban Ramirez <jaime.ramirez@students.fhnw.ch>
2019-02-10 14:44:58 +0100

    Update bpmn_modeler.sql
    change DB Tables

--- SQL/bpmn_modeler.sql
+++ SQL/bpmn_modeler.sql
@@ -1,28 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 4.8.3
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jan 29, 2019 at 09:00 PM
--- Server version: 10.1.36-MariaDB
--- PHP Version: 7.2.11
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `bpmn_modeler`
---
-
--- --------------------------------------------------------
 
 --
 -- Table structure for table `files`
@@ -96,7 +75,3 @@ ALTER TABLE `files`
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
